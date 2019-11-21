57dc00d2abcbe0a7ef98fc597777cc5e065af6c7
DeepakTSeervi <43872250+DeepakTSeervi@users.noreply.github.com>
2019-11-21 15:31:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
 import os
-from googlesearch import search
 import re
+# trying to import few modules
+# if not found, then they are installed using pip
+try:
+    import requests
+except:
+    os.system('pip install requests')
+
+try:
+    from bs4 import BeautifulSoup
+except:
+    os.system('pip install bs4')
+
+try:
+    from googlesearch import search
+except:
+    os.system('pip install google')
 
 def scrap_stackoverflow(url):
-    # search for answer section in a particular section
-    page = requests.get(url, headers=headers)
+    # search for answer section in a particular id of a tag
+    page = requests.get(url, headers=headers, timeout=3)
     soup = BeautifulSoup(page.content, 'html.parser')
-#   temp =soup.find('p', attrs={'class': r'answer-*'}).get_text()
     upvote = soup.find(attrs={'class': 'votecell post-layout--left'}).get_text()
     answer = soup.find(attrs ={'class': 'answercell post-layout--right'}).get_text()
     return upvote, answer
-    # print(answers)
 
-os.system('python3 ' + input('Enter the name of the file: ') + ' 2> ./FileResults/error_file')
+os.system('python3 script.py 2> ./FileResults/error_file')
 fileHandle = open( './FileResults/error_file',"r" )
 lineList = fileHandle.readlines()
 fileHandle.close()
@@ -23,18 +34,15 @@ if len(lineList) == 0:
 
 query = lineList[-1]
 # to be given by the user
-headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"}
+headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"}
 
-results_for_the_search = search(query, tld='com', num=10, stop=5, pause=2)
-print(list(results_for_the_search))
-answers = list()
-for url in results_for_the_search:
+answers = []
+for url in search(query, tld='com', num=10, stop=5, pause=3):
     if re.search(r'(.*)https://stackoverflow.com/(.*)', url):
         # all the answers are stored in answers list
         answers.append(scrap_stackoverflow(url))
-    else:
-        # not able to scrape these sites
-        print('some other site which is not available')
+else:
+    print("Sorry! \nWe didn't find answers for your problem")
 
 for u,a in answers:
     print('upvotes: ', u)
