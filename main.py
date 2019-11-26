8a7cf051354ba80df08c939aff5c8d205c736c08
DeepakTSeervi <43872250+DeepakTSeervi@users.noreply.github.com>
2019-11-26 13:16:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,11 @@ def scrap_stackoverflow(url):
 
     return up[0], answer.rstrip('\n')
 
-
-os.system('python3 ' + input('Enter the filename that you want to compile: ') +
-          ' 2> ./FileResults/error_file')
+filename = input('Enter the filename that you want to compile: ')
+if not re.search(r'(.*)\.py', filename):
+    print('File not found!!\nTry giving the entire path of the file.')
+    exit(0)
+os.system('python3 ' + filename + ' 2> ./FileResults/error_file')
 fileHandle = open('./FileResults/error_file', "r")
 lineList = fileHandle.readlines()
 fileHandle.close()
@@ -57,14 +59,13 @@ if len(lineList) == 0:
     exit(0)
 detailed_query = lineList[-1]
 query = detailed_query[:detailed_query.index(':')] + ' in python 3'
-print('the query: ', query)
 # to be given by the user
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"}
 q_answers = []
 s_o_answers = []
 for url in search(query, tld='com', num=10, stop=6, pause=2):
-    if re.search(r'https://stackoverflow.com/(.*)', url):
+    if re.search(r'(.*)https://stackoverflow.com/(.*)', url):
         # all the answers are stored in answers list
         q_answers.append(scrap_stackoverflow_question(url))
         s_o_answers.append(scrap_stackoverflow(url))
@@ -94,4 +95,7 @@ for u, a in s_o_answers:
 
     if i == len(s_o_answers)-1 or input('\nShall I display another result:(y/n) ') == 'n':
         print('We hope you will solve the error')
-        exit(0)
+        try:
+            exit(0)
+        except:
+            quit()
